b5cf0f9d36e840aa4637a0c06f6043c94b1fffb5
Franz Flasch <franz.flasch@gmx.at>
2020-03-25 20:40:47 +0000

    Some fixes to make it work with current linux kernel

--- mmap_alloc.c
+++ mmap_alloc.c
@@ -1,3 +1,4 @@
+
 #include <linux/version.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -21,9 +22,13 @@
  * Authors: Claudio Scordino, Bruno Morelli
  */
 
+static int dma_mask_bit = 32;
+
 /* character device structures */
 static dev_t mmap_dev;
 static struct cdev mmap_cdev;
+static struct class *cl; // Global variable for the device class
+struct device *mydev;
 
 /* methods of the character device */
 static int mmap_open(struct inode *inode, struct file *filp);
@@ -32,10 +37,10 @@ static int mmap_mmap(struct file *filp, struct vm_area_struct *vma);
 
 /* the file operations, i.e. all character device methods */
 static struct file_operations mmap_fops = {
-        .open = mmap_open,
-        .release = mmap_release,
-        .mmap = mmap_mmap,
-        .owner = THIS_MODULE,
+    .open = mmap_open,
+    .release = mmap_release,
+    .mmap = mmap_mmap,
+    .owner = THIS_MODULE,
 };
 
 // length of the two memory areas
@@ -49,129 +54,141 @@ dma_addr_t dma_handle;
 /* character device open method */
 static int mmap_open(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "mmap_alloc: device open\n");
-        return 0;
+    printk(KERN_INFO "mmap_alloc: device open\n");
+    return 0;
 }
 /* character device last close method */
 static int mmap_release(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "mmap_alloc: device is being released\n");
-        return 0;
+    printk(KERN_INFO "mmap_alloc: device is being released\n");
+    return 0;
 }
 
 // helper function, mmap's the allocated area which is physically contiguous
 int mmap_kmem(struct file *filp, struct vm_area_struct *vma)
 {
-        int ret;
-        long length = vma->vm_end - vma->vm_start;
+    int ret;
+    long length = vma->vm_end - vma->vm_start;
 
-        /* check length - do not allow larger mappings than the number of
-           pages allocated */
-        if (length > NPAGES * PAGE_SIZE)
-                return -EIO;
-/* #ifdef ARCH_HAS_DMA_MMAP_COHERENT */
-	if (vma->vm_pgoff == 0) {
-		printk(KERN_INFO "Using dma_mmap_coherent\n");
-		ret = dma_mmap_coherent(NULL, vma, alloc_ptr,
-					dma_handle, length);
-	} else
-/* #else */
-	{
-		printk(KERN_INFO "Using remap_pfn_range\n");
-		vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
-		vma->vm_flags |= VM_IO;
-		printk(KERN_INFO "off=%d\n", vma->vm_pgoff);
-	        ret = remap_pfn_range(vma, vma->vm_start,
-			      PFN_DOWN(virt_to_phys(bus_to_virt(dma_handle))) +
-			      vma->vm_pgoff, length, vma->vm_page_prot);
-	}
-/* #endif */
-        /* map the whole physically contiguous area in one piece */
-        if (ret < 0) {
-		printk(KERN_ERR "mmap_alloc: remap failed (%d)\n", ret);
-		return ret;
-        }
-        
-        return 0;
+    /* check length - do not allow larger mappings than the number of
+        pages allocated */
+    if (length > NPAGES * PAGE_SIZE)
+        return -EIO;
+
+    printk(KERN_INFO "Using dma_mmap_coherent\n");
+    ret = dma_mmap_coherent(mydev, vma, alloc_ptr, dma_handle, length);
+
+    if (ret < 0) {
+        printk(KERN_ERR "mmap_alloc: remap failed (%d)\n", ret);
+        return ret;
+    }
+
+    return 0;
 }
 
 /* character device mmap method */
 static int mmap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
-	printk(KERN_INFO "mmap_alloc: device is being mapped\n");
-        return mmap_kmem(filp, vma);
+    printk(KERN_INFO "mmap_alloc: device is being mapped\n");
+    return mmap_kmem(filp, vma);
 }
 
 /* module initialization - called at module load time */
 static int __init mmap_alloc_init(void)
 {
-        int ret = 0;
-        int i;
+    int ret = 0;
+    int i;
 
-	/* Allocate not-cached memory area with dma_map_coherent. */
-	printk(KERN_INFO "Use dma_alloc_coherent\n");
-	alloc_ptr = dma_alloc_coherent (NULL, (NPAGES + 2) * PAGE_SIZE,
-					&dma_handle, GFP_KERNEL);
+    /* get the major number of the character device */
+    if ((ret = alloc_chrdev_region(&mmap_dev, 0, 1, "mmap_alloc")) < 0) {
+        printk(KERN_ERR "mmap_alloc: could not allocate major number for mmap\n");
+        goto out;
+    }
 
-	if (!alloc_ptr) {
-                printk(KERN_ERR
-		    "mmap_alloc: dma_alloc_coherent error\n");
-                ret = -ENOMEM;
-                goto out;
+    if (IS_ERR(cl = class_create(THIS_MODULE, "mmap_alloc")))
+    {
+        ret = PTR_ERR(cl);
+        goto out_unalloc_region;
+    }
+
+    if (IS_ERR(mydev = device_create(cl, NULL, mmap_dev, NULL, "mmap_alloc")))
+    {
+        ret = PTR_ERR(mydev);
+        goto out_class_destroy;
+    }
+
+    /* initialize the device structure and register the device with the
+    * kernel */
+    cdev_init(&mmap_cdev, &mmap_fops);
+    if ((ret = cdev_add(&mmap_cdev, mmap_dev, 1)) < 0) {
+        printk(KERN_ERR "mmap_alloc: could not allocate chrdev for mmap\n");
+        goto out_device_destroy;
+    }
+
+    if (mydev->dma_mask == NULL) {
+        mydev->dma_mask = &mydev->coherent_dma_mask;
+    }
+    /*
+    * set this->dma_mask
+    */
+    if (*mydev->dma_mask == 0) {
+        if (dma_set_mask(mydev, DMA_BIT_MASK(dma_mask_bit)) == 0) {
+            dma_set_coherent_mask(mydev, DMA_BIT_MASK(dma_mask_bit));
+        } else {
+            printk(KERN_WARNING "dma_set_mask(DMA_BIT_MASK(%d)) failed\n", dma_mask_bit);
+            dma_set_mask(mydev, DMA_BIT_MASK(32));
+            dma_set_coherent_mask(mydev, DMA_BIT_MASK(32));
+            ret = -ENOMEM;
+            goto out_device_destroy;
         }
-	printk(KERN_INFO "mmap_alloc: physical address is %lu\n",
-	    dma_handle);
-	printk(KERN_INFO "mmap_alloc: bus_to_virt %lu\n",
-	    virt_to_phys(bus_to_virt(dma_handle)));
+    }
 
-	alloc_area = alloc_ptr;
+    printk(KERN_INFO "DMA Mask %llx\n", *mydev->dma_mask);
 
-        /* get the major number of the character device */
-        if ((ret = alloc_chrdev_region(&mmap_dev, 0, 1, "mmap_alloc")) < 0) {
-                printk(KERN_ERR
-		    "mmap_alloc: could not allocate major number for mmap\n");
-                goto out_vfree;
-        }
+    /* Allocate not-cached memory area with dma_map_coherent. */
+    printk(KERN_INFO "Use dma_alloc_coherent\n");
+    alloc_ptr = dma_alloc_coherent (mydev, (NPAGES + 2) * PAGE_SIZE, &dma_handle, GFP_KERNEL);
 
-        /* initialize the device structure and register the device with the
-	 * kernel */
-        cdev_init(&mmap_cdev, &mmap_fops);
-        if ((ret = cdev_add(&mmap_cdev, mmap_dev, 1)) < 0) {
-                printk(KERN_ERR
-		    "mmap_alloc: could not allocate chrdev for mmap\n");
-                goto out_unalloc_region;
-        }
+    if (!alloc_ptr) {
+        printk(KERN_ERR "mmap_alloc: dma_alloc_coherent error\n");
+        ret = -ENOMEM;
+        goto out_device_destroy;
+    }
+    printk(KERN_INFO "mmap_alloc: physical address is %u\n", dma_handle);
 
-	/* store a pattern in the memory.
-	 * the test application will check for it */
-        for (i = 0; i < (NPAGES * PAGE_SIZE / sizeof(int)); i += 2) {
-                alloc_area[i] = (0xdead << 16) + i;
-                alloc_area[i + 1] = (0xbeef << 16) + i;
-        }
-        
-        return ret;
-        
-  out_unalloc_region:
+    alloc_area = alloc_ptr;
+
+    /* store a pattern in the memory.
+    * the test application will check for it */
+    for (i = 0; i < (NPAGES * PAGE_SIZE / sizeof(int)); i += 2) {
+        alloc_area[i] = (0xdead << 16) + i;
+        alloc_area[i + 1] = (0xbeef << 16) + i;
+    }
+
+    return ret;
+
+    out_device_destroy:
+        device_destroy(cl, mmap_dev);
+    out_class_destroy:
+        class_destroy(cl);
+    out_unalloc_region:
         unregister_chrdev_region(mmap_dev, 1);
-  out_vfree:
-	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
-	    dma_handle);
-  out:
+    out:
         return ret;
 }
 
 /* module unload */
 static void __exit mmap_alloc_exit(void)
 {
-        int i;
+    /* remove the character deivce */
+    cdev_del(&mmap_cdev);
+    device_destroy(cl, mmap_dev);
+    class_destroy(cl);
+    unregister_chrdev_region(mmap_dev, 1);
 
-        /* remove the character deivce */
-        cdev_del(&mmap_cdev);
-        unregister_chrdev_region(mmap_dev, 1);
-
-	/* free the memory areas */
-	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
-	    dma_handle);
+    /* free the memory areas */
+    dma_free_coherent (mydev, (NPAGES + 2) * PAGE_SIZE, alloc_ptr, dma_handle);
+    printk(KERN_INFO "mmap_alloc unregistered\n");
 }
 
 module_init(mmap_alloc_init);
@@ -179,4 +196,3 @@ module_exit(mmap_alloc_exit);
 MODULE_DESCRIPTION("mmap_alloc driver");
 MODULE_AUTHOR("Claudio Scordino and Bruno Morelli");
 MODULE_LICENSE("GPL");
-
